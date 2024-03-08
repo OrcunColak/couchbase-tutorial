dc89521d160c8a0566b8010f4648d6cb5569504f
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-08 15:56:37 +0300

    client example

---
+++ src/main/java/com/colak/CouchbaseDataPopulator.java
@@ -0,0 +1,43 @@
+package com.colak;
+
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.Collection;
+import com.couchbase.client.java.json.JsonObject;
+import com.couchbase.client.java.manager.collection.CollectionManager;
+import com.couchbase.client.java.manager.collection.CollectionSpec;
+
+public class CouchbaseDataPopulator {
+
+    private static final int ITEM_COUNT = 1_000;
+
+    String clusterAddress = "localhost"; // Use the actual address if not running locally
+    String username = "admin";
+    String password = "password";
+
+    String bucketName = "mybucket";
+
+    public static void main(String[] args) {
+
+        CouchbaseDataPopulator couchbaseDataPopulator = new CouchbaseDataPopulator();
+        couchbaseDataPopulator.insertDocuments("items-1");
+        couchbaseDataPopulator.insertDocuments("items-2");
+    }
+
+    private void insertDocuments(String collectionName) {
+        // Connect to the Couchbase cluster
+        try (Cluster cluster = Cluster.connect(clusterAddress, username, password)) {
+            Bucket bucket = cluster.bucket(bucketName);
+
+            CollectionManager collectionMgr = bucket.collections();
+            CollectionSpec spec = CollectionSpec.create(collectionName);
+            collectionMgr.createCollection(spec);
+            Collection collection = bucket.collection(collectionName);
+            for (int i = 0; i < ITEM_COUNT; i++) {
+                String id = collectionName + "-id-" + i;
+                JsonObject jsonObject = JsonObject.create().put("value", collectionName + "-value-" + i);
+                collection.insert(id, jsonObject);
+            }
+        }
+    }
+}
